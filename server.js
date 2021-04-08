b5402ca36ffcf38d859140608e5e393b544e2279
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-04-07 23:43:50 -0700

    Adding comments to server.js
    Took a while for me to figure out how to do a pull request, I was commenting out each line in a different file where I followed along from scratch (twice!). Thank you Leon!!

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
+// to do app steps:
+// mkdir
+// open in VS code, open terminal
+// npm init - press enter through - this creates the package.json file
+// npm express --save
+// npm nomgodb --save
+// npm ejs --save
+// npm dotenv --save
+// (can also use shorthand - npm i ejs dotenv --save)
+// mkdir two folders: public, and views
+// touch server.js
+// next steps.....
+
+
+
+
+
+
+// require the use of express
 const express = require('express')
+// define express as 'app' so further references to express can be called as 'app'
 const app = express()
+// require the use of MongoDB
 const MongoClient = require('mongodb').MongoClient
+// choose a port (any number)
 const PORT = 2121
 require('dotenv').config()
+
+// Get connection string from MongoDBAtlas, after creating a cluster, new username/pass, make sure IP address connected
+// change the username/pass and database names in your string
+
+// db is the connection to the database.  it needs the connection string and name
+
+// let can be used once with all variables underneath
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
+
+
+// MongoClient is taking in the connection string as above, and also unifiedTopology: true, which is a special Mongo thing to prevent errors.
+// MongoClient is a promise. When we get the response back from Mongo Atlas, we get a .then (async/await is also possible, depending - .then is the old way)
+// inside the .then is the connection to our client, the connection to our database
+// client is holding the connection. this .then is going to fire once it resolves/connected
+// console log is just to see if it's connected. template literal the dbname 
+// db = client (what we got back) - connected to db, able to grab that connection and store it in the db variable.
+// this is cool - we are storing the connection in the db variable. db anywhere else in our codebase is the connection to our database.
 
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
@@ -68,6 +106,9 @@ app.delete('/deleteTodo', (req, res)=>{
     })
     .catch( err => console.log(err))
 })
+
+// this is starting the server, with it's declared port with the console log to see if it's connected
+
  
 app.listen(process.env.PORT || PORT, ()=>{
     console.log('Server is running, you better catch it!')
